9d6332ac865b6bff00b11ffabad305e0b428ee1b
Adrian Armando Hinojosa Kú <202011420@urp.edu.pe>
2024-12-06 02:33:29 -0500

    actualizar el título y el texto del botón en el componente NameForm para reflejar la creación de una tarjeta de Navidad

--- src/components/NameForm.jsx
+++ src/components/NameForm.jsx
@@ -14,7 +14,7 @@ const NameForm = () => {
 
   return (
     <div style={{ padding: '20px', textAlign: 'center', color: '#fff' }}>
-      <h1>INGRESA TU NOMBRE</h1>
+      <h1>CREA TU TARJETA DE NAVIDAD</h1>
       <form onSubmit={handlePreview}>
         <input
           type="text"
@@ -24,8 +24,8 @@ const NameForm = () => {
           style={{ padding: '10px', fontSize: '16px', borderRadius: '8px', textAlign: 'center', opacity : '0.7' }}
         />
         <div style={{ padding: '20px' }}/>
-        <button type="submit" style={{ padding: '20px 40px', color: 'green', fontSize: '30px', borderRadius: '8px' }}>
-          Aceptar
+        <button type="submit" style={{ padding: '20px 20px', color: 'green', fontSize: '30px', borderRadius: '8px',  fontFamily: 'Bookman Old Style', opacity:'0.9' }}>
+          CONTINUAR
         </button>
       </form>
       

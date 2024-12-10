4d13387baf03eb1eefbecebf6a8134d6c52e8005
Adrian Armando Hinojosa Kú <112140637+adrian04981@users.noreply.github.com>
2024-12-10 15:56:28 +0000

    ajustar la configuración de Vite para establecer la base de URL en '/navidad3/' para el despliegue

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +9,5 @@ export default defineConfig({
       '@': '/src',
     },
   },
-})
+  base: '/navidad3/', // Set the base URL for deployment
+});

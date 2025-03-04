51052838de0674b0d44d15c314ca95a27cbd3371
@AdrianH0498 <adrian.armando20@gmail.com>
2025-03-04 10:14:57 -0500

    Actualizar la configuración de Firebase con nuevas credenciales y eliminar el módulo de análisis.

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,17 @@
-// Import the functions you need from the SDKs
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from "firebase/database"; // Importa el módulo de Realtime Database
+import { getDatabase } from "firebase/database";
 
-// Tu configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDeRN_5dqpel_D0oA_GnB3A-OZsEWNKlt4",
-  authDomain: "navidad-bsf.firebaseapp.com",
-  databaseURL: "https://navidad-bsf-default-rtdb.firebaseio.com",
-  projectId: "navidad-bsf",
-  storageBucket: "navidad-bsf.firebasestorage.app",
-  messagingSenderId: "1061288161631",
-  appId: "1:1061288161631:web:f324b247a1c5a52f7d86ea",
-  measurementId: "G-3VNMKPCCZ3"
+  apiKey: "AIzaSyDQnVQql70vPitr9xuu0HUmlCvVjUHdjLw",
+  authDomain: "manos-solidarias-bsf.firebaseapp.com",
+  databaseURL: "https://manos-solidarias-bsf-default-rtdb.firebaseio.com",
+  projectId: "manos-solidarias-bsf",
+  storageBucket: "manos-solidarias-bsf.firebasestorage.app",
+  messagingSenderId: "1006157711223",
+  appId: "1:1006157711223:web:959288d37219db4bb18372"
 };
 
-// Inicializa Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const database = getDatabase(app); // Inicializa Realtime Database
+const database = getDatabase(app);
 
-// Exporta el objeto `app` y cualquier servicio necesario
-export { app, analytics, database };
+export { app, database };
